690594257ec07f3bd6f54dadcf6ef2b6629e1c6b
Gatteber <Garrett.Atteberry@gmail.com>
2022-07-04 23:46:40 +0900

    added object creator, played with some ideas on display.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hello, World!");
+
+let myLibrary = [];
+
+//book constructor
+function Book(title, author, pageCount, read, liked) {
+    this.title = title,
+    this.author = author,
+    this.pageCount = pageCount,
+    this.read = read,
+    this.liked = liked
+}
+
+const test = new Book("Lord of the Rings", "JRR Tolkien", "1200 Pages", true, true);
+console.log(test.title);
+myLibrary.push(test);
+console.log(myLibrary[0]);
+document.body.innerHTML = myLibrary[0].title;
