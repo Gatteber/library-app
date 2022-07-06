08162a39d058bbde94c82d6b2520ac51a33675c0
Gatteber <Garrett.Atteberry@gmail.com>
2022-07-06 10:26:10 +0900

    Added modal/add book to library functions.

--- README.md
+++ README.md
@@ -7,3 +7,46 @@ Make some example cards
 New book button w/ form
 add a "read" button that changes the status from read to unread
 
+
+
+append the first card child
+loop through and make title box, user box, and bottom box.
+append final children to each box
+^^ kind of works
+
+
+first loop ok
+second loop we need a unique selector
+idea -> go through the object position?? and add an id
+how do we loop that though?
+
+form:
+id = user-title Book Title: Harry Potter
+Book Author: JK Rowling
+Page Count: 350 (0 if unknown)
+Have you read this book? dropdown???
+Did you like this book? dropdown???
+
+
+
+Add your book!
+addBookToLibrary() {
+    const title = user-title
+    const author = user-author
+    const page
+    const read
+    const like
+    const myNewBook = new Book (title, author, page, read, like)
+    myLibrary.push(myNewBook)
+    console.log(myLibrary[0].title);
+}
+closeModal()
+
+
+book submission done
+constructor done
+form clearing done
+
+loop array to add items
+wire up read/liked
+add clear functionality
--- script.js
+++ script.js
@@ -11,8 +11,113 @@ function Book(title, author, pageCount, read, liked) {
     this.liked = liked
 }
 
+
+
+
+// const addBook = document.querySelector('.add-book');
+// addBook.addEventListener('click', () => {
+//     const addADiv = document.querySelector('.card-body')
+//     const classArray = ["info", "user-response", "bottom-box"];
+//     const classArrayInfo = ["info-title", "info-author", "info-pagecount", "info-read", "info-liked"];
+
+//     const newCard = document.createElement ('div');
+//     newCard.classList.add("card");
+//     addADiv.appendChild(newCard); //append first child
+
+//     for (let i = 0; i < classArray.length; i++) {
+//         const newChild = document.createElement('div');
+//         newCard.appendChild(newChild);
+//         newChild.classList.add(classArray[i]);
+//         //append 3 main children
+//     }
+
+//     //needs unique selector
+//     for (let i = 0; i < classArrayInfo.length; i++) {
+//         const infoAddADiv = document.querySelector('.info');
+//         const newChildInfo = document.createElement('div');
+//         infoAddADiv.appendChild(newChildInfo);
+//         newChildInfo.classList.add(classArrayInfo[i]);
+//         //append remaining children??
+//     }
+
+// })
+
+const openModalButtons = document.querySelectorAll ('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll ('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal') //get closest parent with modal class
+        closeModal(modal);
+    })
+})
+
+function openModal (modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal (modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+//form submission
+const bookSubmission = document.getElementById('submitBook')
+const submissionTitle = document.getElementById('bookName')
+const submissionAuthor = document.getElementById('authorName')
+const submissionPageCount = document.getElementById('pagecount')
+const submissionRead = document.getElementById('bookRead')
+const submissionLiked = document.getElementById('bookLiked')
+bookSubmission.addEventListener("click", () => {
+    const createBook = new Book(submissionTitle.value,submissionAuthor.value, submissionPageCount.value, submissionRead.value, submissionLiked.value)
+    myLibrary.push(createBook)
+    //console.log(myLibrary[0])
+    //reset form (no submission, all data stored locally)
+    document.getElementById('bookName').value='';
+    document.getElementById('authorName').value='';
+    document.getElementById('pagecount').value='';
+    closeModal(modal)
+})
+
+
+// UNUSED CODE
+
+
+// const addBook = document.querySelector('.add-book');
+// addBook.addEventListener('click', () => {
+//     const addADiv = document.querySelector('.card-body')
+//     const newCard = document.createElement('div');
+//     const newTitle = document.createElement('div');
+//     newCard.classList.add('card');
+//     newTitle.classList.add('info');
+//     addADiv.appendChild(newCard);
+//     newCard.appendChild(newTitle);
+// })
+
 // const test = new Book("Lord of the Rings", "JRR Tolkien", "1200 Pages", true, true);
 // console.log(test.title);
 // myLibrary.push(test);
 // console.log(myLibrary[0]);
 // document.body.innerHTML = myLibrary[0].title;
+
+
+//UNUSED CODE END
+
+
+
+//DATA CLASSES AND STUFF
+
+//card | info, user-response, bottom-box | rest
+
+// ["info-title", "info-author", "info-pagecount", "info-read", "info-liked", "user-title", "user-author", "user-pagecount", "user-read", "user-liked", "button-box", "delete-box"]
